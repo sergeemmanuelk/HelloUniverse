557f336b3b8d2249628eeaca6c34e0a2d072c045
sergeemmanuelk <serge_em@outlook.fr>
2022-09-20 19:09:07 +0000

    Update HelloUniverse.java

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,12 +1,15 @@
 public class HelloUniverse {
     public static void main(String[] args){
-        String sentence = "Aux dernières nouvelles, le nombre total de planète dans le système solaire est de : ";
-        System.out.println(sentence);
-        int nbPlanete = 8;
-        System.out.println(nbPlanete);
-        sentence = "Il y'a quelques années cependant, elles etaient au nombre de : ";
-        System.out.println(sentence);
-        nbPlanete = 9;
-        System.out.println(nbPlanete);
+        String sentence = "En %d, les planètes du système solaire etaient au nombre de : %d";
+        short year = 2003;
+        int nbPlanet = 0;
+        if(year < 2006){
+            nbPlanet = 9;
+        }
+
+        if(year >= 2006){
+            nbPlanet = 8;
+        }
+        System.out.printf(sentence, year, nbPlanet);
     }
 }
