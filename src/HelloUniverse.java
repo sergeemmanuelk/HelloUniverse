62ecadb8d9e2b95b86984c8cafd735ed8f2733d1
sergeemmanuelk <serge_em@outlook.fr>
2022-10-31 12:50:50 +0000

    reorganize class

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,47 +1,36 @@
 public class HelloUniverse {
     public static void main(String[] args){
-        Planete mercure = new Planete();
-
-        mercure.name = "Mercure";
+        Planete mercure = new Planete("Mercure");
         mercure.diameter = 4880;
         mercure.matter = "Tellurique";
 
-        Planete venus = new Planete();
-        venus.name = "Venus";
+        Planete venus = new Planete("Venus");
         venus.diameter = 12100;
         venus.matter = "Tellurique";
 
-        Planete terre = new Planete();
-        terre.name = "Terre";
+        Planete terre = new Planete("Terre");
         terre.diameter = 12756;
         terre.matter = "Tellurique";
 
-        Planete mars = new Planete();
-        mars.name = "Mars";
+        Planete mars = new Planete("Mars");
         mars.diameter = 6792;
         mars.matter = "Tellurique";
 
-        Planete jupiter = new Planete();
-        jupiter.name = "Jupiter";
+        Planete jupiter = new Planete("Jupiter");
         jupiter.diameter = 142984;
         jupiter.matter = "Gazeuse";
 
-        Planete saturne = new Planete();
-        saturne.name = "Saturne";
+        Planete saturne = new Planete("Saturne");
         saturne.diameter = 120536;
         saturne.matter = "Gazeuse";
 
-        Planete uranus = new Planete();
-        uranus.name = "Uranus";
+        Planete uranus = new Planete("Uranus");
         uranus.diameter = 51118;
         uranus.matter = "Gazeuse";
 
-        Planete neptune = new Planete();
-        neptune.name = "Neptune";
+        Planete neptune = new Planete("Neptune");
         neptune.diameter = 49532;
         neptune.matter = "Gazeuse";
-
-        Planete planeteX = new Planete();
 
         Atmosphere atmosphereUranus = new Atmosphere();
 
@@ -52,7 +41,6 @@ public class HelloUniverse {
         uranus.atmosphere = atmosphereUranus;
 
         System.out.println(jupiter.name + " est une planète " + jupiter.matter + " avec un diamètre de " + jupiter.diameter + " kilomètres.");
-        System.out.println(planeteX.name + " est une planète " + planeteX.matter + " avec un diamètre de " + planeteX.diameter + " kilomètres.");
 
         System.out.println("Neptune a effectué " + neptune.revolution(-3542) + " autour de son étoile");
 
