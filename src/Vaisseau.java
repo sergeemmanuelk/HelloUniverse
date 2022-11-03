c4628354e5c00450d5247bbadec0ea5b8c139a27
sergeemmanuelk <serge_em@outlook.fr>
2022-11-03 17:48:29 +0000

    Update Vaisseau

--- src/Vaisseau.java
+++ src/Vaisseau.java
@@ -2,4 +2,13 @@ public class Vaisseau {
 
     String type;
     int nbPeople;
+    int shielding;
+    int shieldResistance;
+
+    void enableShield(){
+        System.out.println("Activation du bouclier d'un vaisseau de type "+ type);
+    }
+    void disableShield(){
+        System.out.println("Désactivation du bouclier d'un vaisseau de type "+ type);
+    }
 }
